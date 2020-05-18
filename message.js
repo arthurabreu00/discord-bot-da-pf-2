3bc925ce353497b0ce001417339dcb3030228947
Arthur Tavares <arthurabreu00@gmail.com>
2020-05-18 20:09:49 -0300

    Versao 1.0.0

---
+++ message.js
@@ -0,0 +1,14 @@
+exports.sendDmMessage = (username, motivo) => (
+
+    `Assunto: INTIMAÇÃO PARA COMPARECIMENTO EM AUDIÊNCIA
+ Você foi intimado por: ${motivo}
+    
+Processo nº 140031804697451 – 4231 Vara Penal Autor: Policia Federal Réu: ${username}
+
+Nos termos do art. 455, § 1º do Código de Processo Civil é a presente para INTIMAR Vossa Senhoria para comparecer, na qualidade de testemunha, à audiência que será realizada dia (dia mês e ano), às (horário), na (Vara / Fórum) de (Cidade), localizada na (endereço completo).
+
+Caso Vossa Senhoria deixe de comparecer sem motivo justificado será conduzido coercitivamente e responderá pelas despesas do adiamento (art. 455, § 5º do Código de Processo Civil).
+
+Qualquer dúvida poderá ser sanada através dos telefones constantes no rodapé deste documento.
+
+Nome do (a) Advogado seila (a) OAB/38 557238100`);
